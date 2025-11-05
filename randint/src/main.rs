ba54f40ebcbbcadd4a730dfa2eb9a2b8ee273c36
teewa56 <ogunodemarvellous@gmail.com>
2025-11-05 11:13:01 +0100

    fixed the game for the random number guessing

--- randint/src/main.rs
+++ randint/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::stdin;
+use rand::Rng;
+
+fn main(){
+    println!("Guess the number");
+
+    let number = rand::thread_rng().gen_range(1..100);
+
+    let mut guess = String::new();
+
+    stdin()
+        .read_line(&mut guess)
+        .expect("Failed to reada line");
+
+    println!("You guessed: {}", guess);
+    
+    if guess.trim() == number.to_string() {
+        println!("You win!");
+    } else {
+        println!("You lose! The number was: {}", number);
+    }
 }
